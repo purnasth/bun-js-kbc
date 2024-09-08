d629f25dbcd3d361b0157623f3fac6069ce54207
Purna Shrestha <crush9918@gmail.com>
2024-09-08 09:33:00 +0545

    feat: Learn browser run --watch

--- index.ts
+++ index.ts
@@ -1,8 +1,51 @@
-function sayHello(name: string): string {
-  return `Hello, ${name}!`;
-}
+// function sayHello(name: string): string {
+//   return `Hello, ${name}!`;
+// }
 
-console.log(sayHello("Purna"));
+// console.log(sayHello("Purna"));
 
-// print env
-console.log(process.env.BUN_ENV);
+// // print env
+// console.log(process.env.BUN_ENV);
+
+// //? Bun
+
+// let data = Bun.file("README.md");
+// console.log(await data.text());
+
+// // shorthand
+
+// console.log(await Bun.file("README.md").text());
+
+// Bun.serve()
+
+// Bun.serve({
+//   port: 4000,
+//   fetch: function () {
+//     return new Response("Hello, World!");
+//   },
+// });
+
+// Bun.serve({
+//   port: 4000,
+//   fetch(req) {
+//     console.log(req.url);
+//     return new Response("Hello, from Bun --watch");
+//   },
+// });
+
+//? NOTE: to auto update on the browser bun --watch index.ts
+
+Bun.serve({
+  port: 4000,
+  fetch(req) {
+    console.log(req);
+    let url = new URL(req.url);
+    if (url.pathname === "/") {
+      return new Response("Home Page"); 
+    } else if (url.pathname === "/about") {
+      return new Response("About Us");
+    } else {
+      return new Response("404 Not Found", { status: 404 });
+    }
+  },
+});
