7ddc9f6bdb1f3d2d96d81dc02f72e5c225c19cc2
Purna Shrestha <crush9918@gmail.com>
2024-09-08 10:19:25 +0545

    wrapup: bunjs

--- index.ts
+++ index.ts
@@ -52,28 +52,65 @@
 
 // ! Database Connection: SQ Lite
 
-import { Database } from "bun:sqlite";
+// import { Database } from "bun:sqlite";
 
-let db = new Database("mydb.sqlite");
+// let db = new Database("mydb.sqlite");
 
-db.query(
-  "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, address TEXT)"
-).run();
+// db.query(
+//   "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, address TEXT)"
+// ).run();
 
-db.query('INSERT INTO users (name, address) VALUES("Purna", "Kathmandu")').run();
+// db.query('INSERT INTO users (name, address) VALUES("Purna", "Kathmandu")').run();
 
 //? to run the query terminal
-    // sqlite3 mydb.sqlite
-    // SQLite version 3.43.2 2023-10-10 13:08:14
-    // Enter ".help" for usage hints.
-    // sqlite> .tables
-    // users
-    // sqlite> 
+// sqlite3 mydb.sqlite
+// SQLite version 3.43.2 2023-10-10 13:08:14
+// Enter ".help" for usage hints.
+// sqlite> .tables
+// users
+// sqlite>
 
 //? Database is encrypted so cannot be opened via open file .. so we need to extension on vscode to open the file
-    // Name: SQLite
-    // Id: alexcvzz.vscode-sqlite
-    // Description: Explore and query SQLite databases.
-    // Version: 0.14.1
-    // Publisher: alexcvzz
-    // VS Marketplace Link: https://marketplace.visualstudio.com/items?itemName=alexcvzz.vscode-sqlite
+// Name: SQLite
+// Id: alexcvzz.vscode-sqlite
+// Description: Explore and query SQLite databases.
+// Version: 0.14.1
+// Publisher: alexcvzz
+// VS Marketplace Link: https://marketplace.visualstudio.com/items?itemName=alexcvzz.vscode-sqlite
+
+//! Restful API
+
+import { Database } from "bun:sqlite";
+let db = new Database("mydb.sqlite");
+
+Bun.serve({
+  port: 4000,
+  async fetch(req) {
+    let url = new URL(req.url);
+
+    if (url.pathname === "/") {
+      return new Response("Home Page");
+    }
+
+    if (url.pathname === "/users") {
+      let users = db.query("SELECT * FROM users").all();
+      return new Response(JSON.stringify(users), {
+        headers: { "Content-Type": "application/json" },
+      });
+    }
+
+    if (url.pathname === "/create-user" && req.method === "POST") {
+      let body = await req.json();
+      db.query("INSERT INTO users (name, address) VALUES (?, ?)", [
+        body.name,
+        body.address,
+      ]).run();
+      return new Response("User Created", { status: 201 });
+    }
+
+    return new Response("Not Found", { status: 404 });
+  },
+});
+
+
+// ! learn about .prepare and .run
